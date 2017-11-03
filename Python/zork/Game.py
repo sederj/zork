9ccd082764d75dc829dc66c56dadae2b2c9d5c6a
Joseph Seder <sederj@mail.gvsu.edu>
2017-11-03 11:25:51 -0400

    init

---
+++ Python/zork/Game.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 2, 2017
+
+@author: User
+'''
+
+class MyClass(object):
+    """ classdocs """
+    
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ Python/zork/Weapon.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 2, 2017
+
+@author: User
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
