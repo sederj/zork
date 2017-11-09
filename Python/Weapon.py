cb7b78bf85e1e54d7a2a8c706b445c3d0c5a1740
Daniel <Daniel@192.168.1.14>
2017-11-08 19:43:51 -0500

    6:27 11/8

--- Python/Weapon.py
+++ Python/Weapon.py
@@ -1,17 +1,35 @@
+import random
 '''
 Created on Nov 2, 2017
 
 @author: User
 '''
 
-class MyClass(object):
+class Weapon(object):
     '''
     classdocs
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, isDefault):
         '''
         Constructor
         '''
-        
+        if(isDefault == False):
+            randWeap = randrange(0,2)
+            if(randWeap == 0):
+                self.name = "SourStraws"
+                self.attMod = uniform(1,1.75)
+                self.uses = 2
+            elif(randWeap == 1):
+                self.name = "ChocolateBars"
+                self.attMod = uniform(2,2.4)
+                self.uses = 4
+            else:
+                self.name = "NerdBombs"
+                self.attMod = uniform(3.5,5)
+                self.uses = 1
+        else:
+            self.name = "HersheyKisses"
+            self.attMod = 1
+            self.uses = -1
